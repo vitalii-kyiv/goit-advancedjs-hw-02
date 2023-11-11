c50c816940c1bc3e2fcca1d41e5183d54104880e
vitalii-kyiv <v.s.chernous@gmail.com>
2023-11-11 15:50:00 +0200

    tasks_1_3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,22 +6,23 @@ import 'izitoast/dist/css/iziToast.min.css';
 const startBtn = document.querySelector('button[data-start]');
 const getTimeDate = document.querySelector('#datetime-picker');
 
+let selectedDate = 0;
+let currentDate = 0;
+let timeDifference = 0;
+
 const elements = {
   days: document.querySelector('span[data-days]'),
   hours: document.querySelector('span[data-hours]'),
   minutes: document.querySelector('span[data-minutes]'),
   seconds: document.querySelector('span[data-seconds]'),
 };
-console.log(elements.seconds);
 
 startBtn.disabled = true;
 
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
 }
-let selectedDate = 0;
-let currentDate = 0;
-let timeDifference = 0;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -33,12 +34,15 @@ const options = {
     timeDifference = selectedDate - currentDate;
     if (selectedDate > currentDate) {
       startBtn.disabled = false;
-      console.log(convertMs(timeDifference));
     } else {
       iziToast.warning({
+        closeOnEscape: true,
+        closeOnClick: true,
+        backgroundColor: 'tomato',
+        messageColor: 'white',
         position: 'topRight',
-        title: 'Error',
-        message: 'Illegal operation',
+        messageSize: '24',
+        message: 'Please choose a date in the future',
       });
     }
   },
@@ -47,19 +51,13 @@ const options = {
 flatpickr(getTimeDate, options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -70,16 +68,12 @@ startBtn.addEventListener('click', onStartBtnClick);
 let second = 1000;
 
 function onStartBtnClick() {
-  console.log(timeDifference);
-
   const timerInterval = setInterval(function () {
     const timeObj = convertMs(timeDifference);
-
     const days = addLeadingZero(timeObj.days);
     const hours = addLeadingZero(timeObj.hours);
     const minutes = addLeadingZero(timeObj.minutes);
     const seconds = addLeadingZero(timeObj.seconds);
-
     elements.days.textContent = days;
     elements.hours.textContent = hours;
     elements.minutes.textContent = minutes;
@@ -87,9 +81,7 @@ function onStartBtnClick() {
 
     if (timeDifference <= 0) {
       clearInterval(timerInterval);
-      // Додаткова логіка для завершення таймера
     }
-
-    timeDifference -= second; // зменшення різниці на 1 секунду
-  }, second); // оновлення кожну секунду
+    timeDifference -= second;
+  }, second);
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const { delay, step, amount } = form.elements;
+
+form.addEventListener('submit', onSubmitBtnClick);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmitBtnClick(evt) {
+  evt.preventDefault();
+  const firstDelay = Number(delay.value);
+  const delayStep = Number(step.value);
+  const amountPromise = Number(amount.value);
+  for (let i = 1; i <= amountPromise; i++) {
+    const currentDelay = firstDelay + (i - 1) * delayStep;
+
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          closeOnEscape: true,
+          closeOnClick: true,
+          backgroundColor: 'green',
+          messageColor: 'white',
+          position: 'topRight',
+          messageSize: '24',
+          message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          closeOnEscape: true,
+          closeOnClick: true,
+          backgroundColor: 'tomato',
+          messageColor: 'white',
+          position: 'topRight',
+          messageSize: '24',
+          message: `❌ Rejected promise ${position} in ${delay}ms`,
+        });
+      });
   }
 }
